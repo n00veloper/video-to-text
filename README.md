0c38511ac6fc2abc872704bca22639403934f12b
n00veloper <75026881+n00veloper@users.noreply.github.com>
2021-10-06 12:08:02 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ step 4 - colors: "y" will make a grid of 6x6 pixels (dots) and they will have a 
 
 ps: if you have a file named frames, don't run in the same directory, all of its contents will be deleted (for video creation we stract images and then edit they)
 and the program is not threaded, so it can take a while to end.
+
+# threaded version
+step 1 - path to file
+
+step 2 - frames per second (input shold be float between 1 and 0.infinity)
+
+step 3 - max threads
+
+step 4 - output name
+
+step 5 - colors: "y" will make a grid of 6x6 pixels (dots) and they will have a color, anything else will make the same, but whiout color
+
+
+#about fps:
+
+the fps float works as a division for the frames, how lower, is how much frame per second you will get
+example: 0.0069 you will get around 144 frames per second, because 1 / 0.0069 gives the same result.
---
+++ scripts/dotfy - threaded.py
@@ -0,0 +1,169 @@
+import time
+
+import cv2
+import os
+import numpy as np
+from PIL import Image
+path = input("video path:... ")
+vidcap = cv2.VideoCapture(path)
+import threading
+def getframe(time):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC, time*1000)
+    hasframes, image = vidcap.read()
+    if hasframes:
+        if not os.path.isdir("./frames"):
+            os.mkdir("./frames")
+        cv2.imwrite("./frames/image"+str(count)+".jpg", image)
+        return hasframes
+sec = 0
+count = 1
+fps = float(input("frames per second "))
+threads = int(input("max threads: "))
+pathOut = input("video name?: ")
+colorfull = input("colors (y/n)")
+success = getframe(sec)
+while success:
+    count += 1
+    sec += fps
+    sec = round(sec, 2)
+    success = getframe(sec)
+
+def edit(file, color = False):
+    with Image.open(file) as size:
+        width, height = size.size
+        data = np.zeros((height, width, 3), dtype=np.uint8)
+        size = size.convert("RGB")
+        pos = [0,0]
+        while True:
+            red, green, blue = size.getpixel((pos[0], pos[1]))
+            b = [0, 0, 0]
+            w = [255, 255, 255]
+            ii = 0
+            old = pos[0]
+            jmp = 6
+            end = jmp * jmp
+            if red+green+blue >= 0:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    b,b,b,b,b,b,
+                    b,b,b,b,b,b,
+                    b,b,w,w,b,b,
+                    b,b,w,w,b,b,
+                    b,b,b,b,b,b,
+                    b,b,b,b,b,b
+                ]
+            if red+green+blue > 128:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    b,b,b,b,b,b,
+                    b,w,b,b,w,b,
+                    b,b,w,w,b,b,
+                    b,b,w,w,b,b,
+                    b,w,b,b,w,b,
+                    b,b,b,b,b,b
+                ]
+            if red+green+blue > 255:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    b,b,b,b,b,b,
+                    b,w,w,w,w,b,
+                    b,w,w,w,w,b,
+                    b,w,w,w,w,b,
+                    b,w,w,w,w,b,
+                    b,b,b,b,b,b
+                ]
+            if red+green+blue > 383:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    b,b,w,w,b,b,
+                    b,w,w,w,w,b,
+                    w,w,b,b,w,w,
+                    w,w,b,b,w,w,
+                    b,w,w,w,w,b,
+                    b,b,w,w,b,b
+                ]
+            if red+green+blue > 510:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    w,b,w,w,b,w,
+                    b,w,w,w,w,b,
+                    w,w,b,b,w,w,
+                    w,w,b,b,w,w,
+                    b,w,w,w,w,b,
+                    w,b,w,w,b,w
+                ]
+            if red+green+blue > 638:
+                w = [150, 150, 150]
+                if color:
+                    w = [red, green, blue]
+                draw = [
+                    b,b,w,w,b,b,
+                    b,w,w,w,w,b,
+                    w,w,w,w,w,w,
+                    w,w,w,w,w,w,
+                    b,w,w,w,w,b,
+                    b,b,w,w,b,b
+                ]
+            while ii < end:
+                data[pos[1]-1:pos[1]+1, pos[0]-1:pos[0]+1] = draw[ii]
+                pos[0] += 1
+                if pos[0] > old + jmp -1:
+                    pos[1] += 1
+                    pos[0] -= jmp
+                ii += 1
+            pos[0] += 6
+            if pos[0] < width:
+                pos[1] -= jmp
+            if pos[0] >= width:
+                pos[0] = 0
+                pos[1] += jmp
+            if pos[1] >= height:
+                break
+    img = Image.fromarray(data, 'RGB')
+    img.save(file)
+
+pathIn = "./frames"
+frame_array = []
+first = True
+trd = []
+for i in range(1, count):
+    filename = pathIn + "/image" + str(i) + ".jpg"
+    if colorfull == "y":
+        trd.append(threading.Thread(target=edit, args=(filename,True,)))
+    else:
+        trd.append(threading.Thread(target=edit, args=(filename,)))
+    while threading.active_count() > threads:
+        time.sleep(1)
+    trd[-1].start()
+for i in range(1, count):
+    filename = pathIn+"/image"+str(i)+".jpg"
+    print(str(i)+" out of "+str(count))
+    trd[i-1].join()
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    frame_array.append(img)
+    size = (width, height)
+    if first:
+        out = cv2.VideoWriter(pathOut, cv2.VideoWriter_fourcc(*"mp4v"), fps / (fps * fps), size)
+        first = False
+    if len(frame_array) > 10:
+        for i in range(len(frame_array)):
+            out.write(frame_array[i])
+        frame_array = []
+if len(frame_array) > 0:
+    for i in range(len(frame_array)):
+        out.write(frame_array[i])
+    frame_array = []
+out.release()
+for f in os.listdir(pathIn):
+    os.remove(os.path.join(pathIn, f))
